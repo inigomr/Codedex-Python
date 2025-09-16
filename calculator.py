6a04d17669bdb2d5a5536323dee32c7dce07e86d
inigoromano <inigo.manuelromano@gmail.com>
2025-09-16 18:03:16 +1200

    Add birthday message generator and basic calculator functions

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add(a, b):
+  sum = a + b
+  return sum
+
+def subtract(a, b):
+  difference = a - b
+  return difference
+
+def multiply(a, b):
+  product = a * b
+  return product
+
+def divide(a, b):
+  quotient = a / b
+  return quotient
+
+def exp(a, b):
+  result = a ** b
+  return result
+
+print(add(2, 4))
+print(subtract(2, 4))
+print(multiply(2, 4))
+print(divide(2, 4))
+print(exp(2, 4))
---
+++ main.py
@@ -0,0 +1,10 @@
+import datetime, bday_messages
+
+today = datetime.date.today()
+next_birthday = datetime.date(2026, 1, 20)
+days_away = next_birthday - today
+
+if today == next_birthday:
+    print(bday_messages.random_message)
+else:
+    print(f"My next birthday is {days_away.days} days away!")
